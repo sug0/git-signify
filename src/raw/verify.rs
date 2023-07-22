ff4d6337368cc3ac64437c788aef71418e54e5a9
Tiago Carvalho <sugoiuguu@tfwno.gf>
2023-07-22 14:58:50 +0100

    factor out verify fn

--- src/raw/verify.rs
+++ src/raw/verify.rs
@@ -4,16 +4,30 @@ use std::path::PathBuf;
 
 use anyhow::{Context, Result};
 use git2::{Oid, Repository};
-use libsignify::{Codeable, Signature};
+use libsignify::{Codeable, PublicKey, Signature};
 
 use crate::utils;
 
 /// Execute the `verify` command.
 pub fn command(key_path: PathBuf, recover: bool, tree_rev: String) -> Result<()> {
     let repo = Repository::open(".").context("Failed to open git repository")?;
+    let public_key = utils::get_public_key(key_path)?;
+    let recovered_oid = verify(&repo, &public_key, &tree_rev, recover)?;
+    if let Some(recovered_oid) = recovered_oid {
+        println!("{recovered_oid}");
+    }
+    Ok(())
+}
 
+/// Verify the signature under `tree_rev` with the given public key.
+pub fn verify(
+    repo: &Repository,
+    public_key: &PublicKey,
+    tree_rev: &str,
+    recover: bool,
+) -> Result<Option<Oid>> {
     let oid = repo
-        .revparse_single(&tree_rev)
+        .revparse_single(tree_rev)
         .context("Failed to look-up git tree oid")?
         .id();
     let tree = repo
@@ -23,7 +37,7 @@ pub fn command(key_path: PathBuf, recover: bool, tree_rev: String) -> Result<()>
     let object = tree
         .get_name("object")
         .context("Failed to look-up signed object in the tree")?
-        .to_object(&repo)
+        .to_object(repo)
         .context("The signed object could not be retrieved")?;
     let object = object
         .as_blob()
@@ -34,7 +48,7 @@ pub fn command(key_path: PathBuf, recover: bool, tree_rev: String) -> Result<()>
         let signature = tree
             .get_name("signature")
             .context("Failed to look-up signature in the tree")?
-            .to_object(&repo)
+            .to_object(repo)
             .context("The signature object could not be retrieved")?;
         let signature = signature
             .as_blob()
@@ -44,17 +58,12 @@ pub fn command(key_path: PathBuf, recover: bool, tree_rev: String) -> Result<()>
             .context("Failed to parse signature")?
     };
 
-    let public_key = utils::get_public_key(key_path)?;
-
     public_key
         .verify(dereferenced_obj, &signature)
         .map_err(utils::Error::new)
         .context("Failed to verify signature")?;
 
-    if recover {
-        let oid = Oid::from_bytes(dereferenced_obj).context("Failed to parse git object id")?;
-        println!("{oid}");
-    }
-
-    Ok(())
+    recover
+        .then(|| Oid::from_bytes(dereferenced_obj).context("Failed to parse git object id"))
+        .transpose()
 }
