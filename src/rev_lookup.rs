2c44a75e37223c0b28275d32108f35b808047a70
Tiago Carvalho <sugoiuguu@tfwno.gf>
2025-10-19 18:26:04 +0100

    parse oid once

--- src/rev_lookup.rs
+++ src/rev_lookup.rs
@@ -10,12 +10,12 @@ use crate::utils;
 /// Execute the `rev-lookup` command.
 pub fn command(key_path: PathBuf, rev: String) -> Result<()> {
     let repo = utils::open_repository()?;
+    let object_oid = repo
+        .revparse_single(&rev)
+        .context("Failed to look-up git object")?
+        .id();
     for public_key in utils::get_public_keys(key_path)?.into_values() {
         let tree_rev = {
-            let object_oid = repo
-                .revparse_single(&rev)
-                .context("Failed to look-up git object")?
-                .id();
             let key_fingerprint = public_key.fingerprint()?;
             utils::craft_signature_reference(key_fingerprint, object_oid)
         };
